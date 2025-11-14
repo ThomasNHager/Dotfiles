82335cea5e992e69361309268a6810ba53d7cc74
Tommy <tommy.hager@yale.edu>
2025-11-13 23:37:42 -0500

    Some additional config tweaks with the system update

--- .config/polybar/start.sh
+++ .config/polybar/start.sh
@@ -3,5 +3,5 @@
 source ~/.virtualenvs/Polybar/bin/activate
 feh --bg-scale ~/Desktop/Backgrounds/jellyfish.jpg
 killall polybar
+bash /home/thager/.config/polybar/full.sh 2>&1 | tee -a /tmp/full.log & disown
 polybar --reload horizontal 2>&1 | tee -a /tmp/polybar.log & disown
-# polybar --reload vertical 2>&1 | tee -a /tmp/polybar.log & disown
