7fff6c13257d59e34d6653f6e1397f658cb49b23
Tommy <tommy.hager@yale.edu>
2025-11-13 20:28:53 -0500

    Updated dotfiles for Debian 13

--- .config/polybar/start.sh
+++ .config/polybar/start.sh
@@ -4,4 +4,4 @@ source ~/.virtualenvs/Polybar/bin/activate
 feh --bg-scale ~/Desktop/Backgrounds/jellyfish.jpg
 killall polybar
 polybar --reload horizontal 2>&1 | tee -a /tmp/polybar.log & disown
-polybar --reload vertical 2>&1 | tee -a /tmp/polybar.log & disown
+# polybar --reload vertical 2>&1 | tee -a /tmp/polybar.log & disown
