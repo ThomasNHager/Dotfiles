8462bbbced1530281fcb1f13548c433a15284fac
Tommy <tommy.hager@yale.edu>
2026-03-29 20:26:57 -0400

    Removed plugins from Nvim that I don't use on Linux

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -8,18 +8,9 @@ return{
     {
         "hrsh7th/nvim-cmp",
         dependencies = {
-            {
-                "MattiasMTS/cmp-dbee",
-                dependencies = {
-                    {"kndndrj/nvim-dbee"}
-                },
-                ft = "sql", -- optional but good to have
-                opts = {}, -- needed
-            },
         },
         opts = {
             sources = {
-                { "cmp-dbee" },
             },
         },
     }
